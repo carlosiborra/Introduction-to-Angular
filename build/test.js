e8759f4b7c0cd0f284da624224b9588d5b2a3b8b
100451170 <100451170@alumnos.uc3m.es>
2022-06-11 14:11:32 +0200

    2nd Commit Modified build folder

--- build/test.js
+++
@@ -1,7 +0,0 @@
-console.log("Hola mundo");
-// Compile
-// cmd: cd Desktop\Code\Angular\Curso - Udemy
-// cmd: tsc prueba.ts
-// then only: tsc
-// Execute: node prueba.js
-// >> Hola mundo
--- code/build/test.js
+++ code/build/test.js
@@ -1,9 +1,7 @@
 console.log("Hola mundo");
-
 // Compile
 // cmd: cd Desktop\Code\Angular\Curso\Code
 // cmd: tsc test.ts
 // then only: tsc
-
 // Execute: node test.js
 // >> Hola mundo
